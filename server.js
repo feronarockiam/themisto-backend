0a7b0954377f420b32e33f09277045e86ef23607
feronarockiam <feronarockiam1493@gmail.com>
2023-07-16 19:15:20 +0530

    lead

--- server.js
+++ server.js
@@ -1,19 +1,76 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = 3000;
-const url = 'mongodb+srv://themisto:12345@cluster0.wcoy5xl.mongodb.net/?retryWrites=true&w=majority';
-const dbName = 'Test';
+const url = 'mongodb+srv://themisto:12345@cluster0.wcoy5xl.mongodb.net/Test?retryWrites=true&w=majority';
 
 app.use(express.json());
+
+let client;
+
+async function connectToDatabase() {
+  try {
+    client = await MongoClient.connect(url);
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Failed to connect to the database:', error);
+    throw error;
+  }
+}
+
+connectToDatabase();
+
+async function authenticateUser(username, password) {
+  const db = client.db();
+  const collection = db.collection('user_cred');
+
+  const user = await collection.findOne({ UserName: username, Password: password });
+
+  if (!user) {
+    throw new Error('Invalid credentials');
+  }
+
+  return user;
+}
+
+async function fetchLeadsBySocialMediaEngagement(socialMediaEngagement) {
+  const db = client.db();
+  const collection = db.collection('lead');
+
+  const query = { 'Social Media Engagement': { $eq: socialMediaEngagement } };
+
+  const leads = await collection.find(query).toArray();
+
+  return leads;
+}
+
+async function fetchLeadsByOrderFrequency(orderFreq) {
+  const db = client.db();
+  const collection = db.collection('lead');
+
+  const query = { 'Order Frequency': { $eq: orderFreq } };
+
+  const leads = await collection.find(query).toArray();
+
+  return leads;
+}
+
+async function fetchSchedule() {
+  const db = client.db();
+  const collection = db.collection('schedule');
+
+  const schedule = await collection.find().limit(10).toArray();
+
+  return schedule;
+}
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
 
   authenticateUser(username, password)
     .then(user => {
-      res.json({ message: 200 })
+      res.json({ message: 'Authentication successful' });
     })
     .catch(err => {
       console.error('Error occurred:', err);
@@ -21,13 +78,12 @@ app.post('/login', (req, res) => {
     });
 });
 
-
 app.post('/leadsSocial', (req, res) => {
   const socialMediaEngagementLevel = req.body.socialMediaEngagement;
 
-  Social(socialMediaEngagementLevel)
-    .then(rows => {
-      res.json(rows);
+  fetchLeadsBySocialMediaEngagement(socialMediaEngagementLevel)
+    .then(leads => {
+      res.json(leads);
     })
     .catch(err => {
       console.error('Error occurred:', err);
@@ -35,13 +91,12 @@ app.post('/leadsSocial', (req, res) => {
     });
 });
 
-
 app.post('/leadsOrder', (req, res) => {
   const orderFreq = req.body.orderFreq;
 
-  Order(orderFreq)
-    .then(rows => {
-      res.send(rows);
+  fetchLeadsByOrderFrequency(orderFreq)
+    .then(leads => {
+      res.json(leads);
     })
     .catch(err => {
       console.error('Error occurred:', err);
@@ -60,85 +115,109 @@ app.get('/schedule', (req, res) => {
     });
 });
 
-async function fetchSchedule() {
-  const client = new MongoClient(url);
+// ...
+
+app.post('/api/fetch', async (req, res) => {
+  console.log(req.body);
+  const { condition, number, column } = req.body;
+
+  let operator;
+  switch (condition) {
+    case 'more':
+      operator = '$gt';
+      break;
+    case 'less':
+      operator = '$lt';
+      break;
+    case 'equal':
+      operator = '$eq';
+      break;
+    default:
+      return res.status(400).json({ error: 'Invalid condition' });
+  }
+
+  const db = client.db();
+  const collection = db.collection('lead');
+
+  // Add symbols to the value if the column is 'Average Order Value' or 'Cart Abandonment Rate'
+  let value;
+  if (column === 'Average Order Value') {
+    value = `$${number}`;
+  } else if (column === 'Cart Abandonment Rate') {
+    value = `${number}%`;
+  } else {
+    value = number;
+  }
+
+  const query = {
+    [column]: {
+      [operator]: value,
+    },
+  };
 
   try {
-    await client.connect();
-
-    const db = client.db(dbName);
-    const collection = db.collection('schedule');
-
-    const schedule = await collection.find().limit(10).toArray();
-
-    return schedule;
-  } finally {
-    await client.close();
+    const documents = await collection.find(query).toArray();
+    res.send(documents);
+  } catch (err) {
+    console.error('Error occurred:', err);
+    res.status(500).json({ error: 'Failed to fetch data' });
   }
-}
+});
 
-async function authenticateUser(username, password) {
-  const client = new MongoClient(url);
+// ...
+
+// ...
+
+// ...
+
+app.post('/fetch/age', async (req, res) => {
+  console.log(req.body);
+  const { condition, number } = req.body;
+  const column = 'Age'; // Assuming the column is 'Age' by default
+
+  const db = client.db();
+  const collection = db.collection('lead');
+
+  let query;
+
+  if (condition === 'older') {
+    query = {
+      $or: [
+        { [column]: { $gte: parseInt(number) } },
+        { [column]: { $regex: `^[0-9]+-[0-9]*$` } }
+      ]
+    };
+  } else if (condition === 'younger') {
+    query = {
+      $or: [
+        { [column]: { $lte: parseInt(number) } },
+        { [column]: { $regex: `^[0-9]*-[0-9]+\$` } }
+      ]
+    };
+  } else if (condition === 'between') {
+    const [min, max] = number.split('-').map(num => parseInt(num.trim()));
+    query = {
+      [column]: {
+        $gte: min,
+        $lte: max,
+      },
+    };
+  } else {
+    return res.status(400).json({ error: 'Invalid condition' });
+  }
 
   try {
-    await client.connect();
-
-    const db = client.db(dbName);
-    const collection = db.collection('user_cred');
-
-    // Find the user by the provided username and password
-    const user = await collection.findOne({ UserName: username, Password: password });
-
-    if (!user) {
-      throw new Error('Invalid credentials');
-    }
-
-    return user;
-  } finally {
-    await client.close();
+    const documents = await collection.find(query).toArray();
+    res.send(documents);
+  } catch (err) {
+    console.error('Error occurred:', err);
+    res.status(500).json({ error: 'Failed to fetch data' });
   }
-}
+});
+
+// ...
 
 
-async function Social(socialMediaEngagement) {
-  const client = new MongoClient(url);
-
-  try {
-    await client.connect();
-
-    const db = client.db(dbName);
-    const collection = db.collection('lead');
-
-    // Modify the query to include the social media engagement level
-    const query = { 'Social Media Engagement': { $eq: socialMediaEngagement } };
-
-    const rows = await collection.find(query).toArray();
-
-    return rows;
-  } finally {
-    await client.close();
-  }
-}
-
-async function Order(orderFreq) {
-  const client = new MongoClient(url);
-
-  try {
-    await client.connect();
-
-    const db = client.db(dbName);
-    const collection = db.collection('lead');
-
-    // Modify the query to include the social media engagement level
-    const query = { 'Order Frequency': { $eq: orderFreq } };
-
-    const rows = await collection.find(query).toArray();
-
-    return rows;
-  } finally {
-    await client.close();
-  }
-}
 
 app.listen(port, () => {
   console.log(`API server listening on port ${port}`);
